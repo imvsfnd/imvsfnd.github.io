a317fe26e040dca7a73c588183588ba2db5369bc
kmon25 <kmon25@gmail.com>
2019-04-18 18:27:41 +0800

    test

--- Web/js/custom.js
+++ Web/js/custom.js
@@ -1,6 +1,23 @@
-$(function() {
+// moment, unique
+var moment = require('moment');
+var unique = require('uniq');
+
+var myDate = new Date();
+var myCoolDate = moment(myDate).format('LL');
+
+var myList = [1, 2, 1, 1, 1, 3, 4, 5, 6, 4, 4, 7];
+
+var myUniqueList = unique(myList);
+
+console.log(myUniqueList);
 
 
+// LAZY LOADING
+// import LazyLoad from "vanilla-lazyload";
+let LazyLoad = require('vanilla-lazyload');
 
+
+const myLazyLoad = new LazyLoad({
+    elements_selector: ".photo"
 });
 
